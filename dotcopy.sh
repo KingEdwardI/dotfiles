b7c791b8edaff4f5a862b4ea3db430c2775da8f1
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-11-12 00:50:55 -0700

    creating dotcopy; removing old files

---
+++ dotcopy.sh
@@ -0,0 +1,5 @@
+cp ~/.vimrc ~/workspace/dotfiles/vimrc
+cp ~/.aliases ~/workspace/dotfiles/aliases
+cp ~/.zshrc ~/workspace/dotfiles/zshrc
+cp -r ~/.vim ~/workspace/dotfiles/vim
+~/.bashrc ~/workspace/dotfiles/bashrc
