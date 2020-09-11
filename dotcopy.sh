1d7f92e43d5732e528168aee634d656ee6b44dd1
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2020-09-11 00:13:42 -0600

    updating dotfiles with better organization and best practice

--- dotcopy.sh
+++
@@ -1,5 +0,0 @@
-cp ~/.vimrc ~/workspace/dotfiles/vimrc
-cp ~/.aliases ~/workspace/dotfiles/aliases
-cp ~/.zshrc ~/workspace/dotfiles/zshrc
-cp -r ~/.vim ~/workspace/dotfiles/vim
-cp ~/.bashrc ~/workspace/dotfiles/bashrc
