e8d88c439a0f345b0d9dd25fcf08d8677ef1a54e
Edward Vetterdrake <edward.vetterdrake@rm-mb-170.extendthereach.com>
2021-03-15 12:06:19 -0600

    adding setup file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# link aliases
+bash ./symlink_aliases.sh
+
+# install vim-plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
